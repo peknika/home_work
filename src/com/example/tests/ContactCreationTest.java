1602720ba6d83ae7e0bf7f6542acb089b858c79b
Veronika Peknaya <peknika@gmail.com>
2015-05-04 20:34:06 +0300

    TestBase was created for ContactCreationTest and GroupCreationTest

--- src/com/example/tests/ContactCreationTest.java
+++ src/com/example/tests/ContactCreationTest.java
@@ -1,30 +1,10 @@
 package com.example.tests;
 
-import java.util.regex.Pattern;
-import java.util.concurrent.TimeUnit;
-
-import static org.junit.Assert.*;
-import static org.hamcrest.CoreMatchers.*;
-
 import org.openqa.selenium.*;
-import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.support.ui.Select;
-import org.testng.annotations.AfterClass;
-import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
-public class ContactCreationTest {
-  private WebDriver driver;
-  private String baseUrl;
-  private boolean acceptNextAlert = true;
-  private StringBuffer verificationErrors = new StringBuffer();
-
-  @BeforeClass
-  public void setUp() throws Exception {
-    driver = new FirefoxDriver();
-    baseUrl = "http://localhost/";
-    driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
-  }
+public class ContactCreationTest extends TestBase {
 
   @Test
   public void testEmtyContactCreation() throws Exception {
@@ -89,9 +69,6 @@ private void goToContactPage() {
 	driver.findElement(By.linkText("add new")).click();
 }
 
-private void openMainPage() {
-	driver.get(baseUrl + "/addressbookv4.1.4/");
-}
 
 private void setKeyInElement(String elementName, String inputValue)
 {
@@ -113,46 +90,4 @@ private void selectDropdownValue (String elementName, String inputValue)
 	new Select(dropdownElement).selectByVisibleText(inputValue);
 }
 
-  @AfterClass
-  public void tearDown() throws Exception {
-    driver.quit();
-    String verificationErrorString = verificationErrors.toString();
-    if (!"".equals(verificationErrorString)) {
-      fail(verificationErrorString);
-    }
-  }
-
-  private boolean isElementPresent(By by) {
-    try {
-      driver.findElement(by);
-      return true;
-    } catch (NoSuchElementException e) {
-      return false;
-    }
-  }
-
-  private boolean isAlertPresent() {
-    try {
-      driver.switchTo().alert();
-      return true;
-    } catch (NoAlertPresentException e) {
-      return false;
-    }
-  }
-
-
-  private String closeAlertAndGetItsText() {
-    try {
-      Alert alert = driver.switchTo().alert();
-      String alertText = alert.getText();
-      if (acceptNextAlert) {
-        alert.accept();
-      } else {
-        alert.dismiss();
-      }
-      return alertText;
-    } finally {
-      acceptNextAlert = true;
-    }
-  }
 }
---
+++ src/com/example/tests/TestBase.java
@@ -0,0 +1,66 @@
+package com.example.tests;
+
+import static org.junit.Assert.fail;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+
+public class TestBase {
+
+	protected static WebDriver driver;
+	private static String baseUrl;
+	private static StringBuffer verificationErrors = new StringBuffer();
+
+	@BeforeTest
+	public void setUp() throws Exception {
+	    driver = new FirefoxDriver();
+	    baseUrl = "http://localhost/";
+	    driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+	  }
+	
+	@AfterTest
+	public void tearDown() throws Exception {
+	    driver.quit();
+	    String verificationErrorString = verificationErrors.toString();
+	    if (!"".equals(verificationErrorString)) {
+	      fail(verificationErrorString);
+	    }
+	  }
+
+	protected void returnToGroupsPage() {
+	    driver.findElement(By.linkText("group page")).click();
+	}
+
+	protected void submitGroupCreation() {
+	    driver.findElement(By.name("submit")).click();
+	}
+
+	protected void fillGroupForm(GroupData group) {
+	    driver.findElement(By.name("group_name")).clear();
+	    driver.findElement(By.name("group_name")).sendKeys(group.name);
+	    driver.findElement(By.name("group_header")).clear();
+	    driver.findElement(By.name("group_header")).sendKeys(group.header);
+	    driver.findElement(By.name("group_footer")).clear();
+	    driver.findElement(By.name("group_footer")).sendKeys(group.footer);
+	}
+
+	protected void initGroupCreation() {
+	    driver.findElement(By.name("new")).click();
+	}
+
+	protected void gotoGroupsPage() {
+	    driver.findElement(By.linkText("groups")).click();
+	}
+
+	protected void openMainPage() {
+		// open main page
+	    driver.get(baseUrl + "/addressbookv4.1.4/");
+	}
+
+
+}
